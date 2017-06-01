654dc0dd40552e4b1ce1792049988c39de96a8d1
CLAY <claypan@foxmail.com>
2017-06-01 11:44:08 +0800

    test tim8 pwm ok

--- App/motor_step5.h
+++ App/motor_step5.h
@@ -7,16 +7,25 @@ B:	PA9		IN
 BN:	PB14	OUT
 C:	PA10	IN
 CN:	PB15	OUT
+#if TIM8_PWM
+D:	PC7		IN
+DN:	PB0	OUT
+E:	PC8		IN
+EN:	PB1		OUT
+#else
 timer 4 for pwm d dn e en
 D:	PB6		IN
 DN:	PB7		OUT
 E:	PB8		IN
 EN:	PB9		OUT
+#endif
 timer 6 for step 
 */
 
 #ifndef MOTOR_STEP5_H_
 #define MOTOR_STEP5_H_ 
+
+#define TIM8_PWM 1
 
 #define PHASE_A_H_ON 		TIM_CCxCmd(TIM1, TIM_Channel_1, TIM_CCx_Enable)
 #define PHASE_A_H_OFF 		TIM_CCxCmd(TIM1, TIM_Channel_1, TIM_CCx_Disable)
@@ -33,6 +42,18 @@ timer 6 for step
 #define PHASE_CN_L_ON 	TIM_CCxNCmd(TIM1, TIM_Channel_3, TIM_CCxN_Enable)
 #define PHASE_CN_L_OFF 	TIM_CCxNCmd(TIM1, TIM_Channel_3, TIM_CCxN_Disable)
 
+
+#if  TIM8_PWM
+#define PHASE_D_H_ON 		TIM_CCxCmd(TIM8, TIM_Channel_2, TIM_CCx_Enable)
+#define PHASE_D_H_OFF 		TIM_CCxCmd(TIM8, TIM_Channel_2, TIM_CCx_Disable)
+#define PHASE_DN_L_ON 	TIM_CCxNCmd(TIM8, TIM_Channel_2, TIM_CCxN_Enable)
+#define PHASE_DN_L_OFF 	TIM_CCxNCmd(TIM8, TIM_Channel_2, TIM_CCxN_Disable)
+
+#define PHASE_E_H_ON 		TIM_CCxCmd(TIM8, TIM_Channel_3, TIM_CCx_Enable)
+#define PHASE_E_H_OFF 		TIM_CCxCmd(TIM8, TIM_Channel_3, TIM_CCx_Disable)
+#define PHASE_EN_L_ON 	TIM_CCxNCmd(TIM8, TIM_Channel_3, TIM_CCxN_Enable)
+#define PHASE_EN_L_OFF 	TIM_CCxNCmd(TIM8, TIM_Channel_3, TIM_CCxN_Disable)
+#else
 #define PHASE_D_H_ON 		TIM_CCxCmd(TIM4, TIM_Channel_1, TIM_CCx_Enable)
 #define PHASE_D_H_OFF 		TIM_CCxCmd(TIM4, TIM_Channel_1, TIM_CCx_Disable)
 #define PHASE_DN_L_ON 	TIM_CCxCmd(TIM4, TIM_Channel_2, TIM_CCx_Enable)
@@ -42,7 +63,9 @@ timer 6 for step
 #define PHASE_E_H_OFF 		TIM_CCxCmd(TIM4, TIM_Channel_3, TIM_CCx_Disable)
 #define PHASE_EN_L_ON 	TIM_CCxCmd(TIM4, TIM_Channel_4, TIM_CCx_Enable)
 #define PHASE_EN_L_OFF 	TIM_CCxCmd(TIM4, TIM_Channel_4, TIM_CCx_Disable)
+#endif
 
+#if 0
 #define PHASE_A_IN_ON 		TIM_CCxCmd(TIM1, TIM_Channel_1, TIM_CCx_Enable)
 #define PHASE_A_IN_OFF 		TIM_CCxCmd(TIM1, TIM_Channel_1, TIM_CCx_Disable)
 #define PHASE_AN_OUT_ON 	TIM_CCxNCmd(TIM1, TIM_Channel_1, TIM_CCxN_Enable)
@@ -67,11 +90,11 @@ timer 6 for step
 #define PHASE_E_IN_OFF 		TIM_CCxCmd(TIM4, TIM_Channel_3, TIM_CCx_Disable)
 #define PHASE_EN_OUT_ON 	TIM_CCxCmd(TIM4, TIM_Channel_4, TIM_CCx_Enable)
 #define PHASE_EN_OUT_OFF 	TIM_CCxCmd(TIM4, TIM_Channel_4, TIM_CCx_Disable)
-
+#endif
 
 //test only
 #define CLK5_PORT	GPIOA
-#define CLK5_PIN		GPIO_Pin_7//12->6
+#define CLK5_PIN		GPIO_Pin_6 //12->7
 #define STEP5_CLK_H()	GPIO_SetBits(CLK5_PORT, CLK5_PIN)
 #define STEP5_CLK_L()	GPIO_ResetBits(CLK5_PORT, CLK5_PIN)
 #define STEP5_CLK_TOGGLE() CLK5_PORT->ODR ^= CLK5_PIN
